6c9df1cca0687d55b30db593b8878f4d8dd93b1a
arektar <zander5@mail.ru>
2017-01-19 15:04:39 +0300

    Update vk_friends_online.py

--- vk_friends_online.py
+++ vk_friends_online.py
@@ -1,45 +1,45 @@
 import vk
-
+import getpass
 
 APP_ID = 5827034
 
 
 def get_user_login():
-    login = str(input("Write your VK.com login: "))
+    login = getpass.getpass("Write your VK.com login: ")
     assert login, 'No login'
     return login
 
 
 def get_user_password():
-    password = str(input("Write your VK.com password: "))
+    password = getpass.getpass("Write your VK.com password: ")
     assert password, 'No password'
-    return
+    return password
 
 
-def get_online_friends(login, password):
+def make_vk_session(login, password):
     session = vk.AuthSession(
         app_id=APP_ID,
         user_login=login,
         user_password=password,
     )
-    api = vk.API(session)  # например, api.friends.get()
-    friends = api.friends.get(fields='online')
-    online_friends = []
-    for friend in friends:
-        if friend['online'] == 1:
-            online_friends.append(friend)
-    return online_friends
+    api = vk.API(session)
+    return api
 
 
-def output_friends_to_console(friends_online):
+def get_online_friends(api):
+    online_friends = api.friends.getOnline()
+    return online_friends['response']
+
+
+def output_friends_to_console(api,friends_online):
     print("Друзья онлайн: \n")
     for friend in friends_online:
+        api.users.get(friend)
         print("%s %s" % (friend['first_name'], friend['last_name']))
-
-    pass
 
 if __name__ == '__main__':
     my_login = get_user_login()
     my_password = get_user_password()
-    my_friends_online = get_online_friends(my_login, my_password)
-    output_friends_to_console(my_friends_online)
+    my_session_api = make_vk_session(my_login, my_password)
+    my_friends_online = get_online_friends(my_session_api)
+    output_friends_to_console(my_session_api, my_friends_online)
